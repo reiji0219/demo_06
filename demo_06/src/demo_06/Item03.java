63bb0a333b1ec2b1bafc8b3beeb3bf58bed44e2a
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-09-03 14:03:50 +0900

    <br/>

--- demo_06/src/demo_06/Item03.java
+++ demo_06/src/demo_06/Item03.java
@@ -8,7 +8,58 @@ import java.util.Iterator;
 import java.util.function.Consumer;
 
 public class Item03 implements Interface
-{
+{	
+	private String id;
+	private String name;
+	private double price;
+	private double tax;
+	private Calc03 calc;
+	
+	
+	public Item03 id( String id ){
+	this.id = id;
+	return this;
+	}
+	public Item03 name( String name ){
+	this.name = name;
+	return this;
+	}
+	public Item03 price( double price ){
+	this.price = price;
+	return this;
+	}
+	public Item03 tax( double tax ){
+	this.tax = tax;
+	return this;
+	}
+	public Item03 calc( Calc03 calc ) {
+		this.calc = calc;
+		return this;
+	}
+	
+	public static void save( Consumer<Item03> con ){
+	Item03 item = new Item03();
+	con.accept( item );
+	indi( "save :" + item );
+	}
+
+	public String getId(){
+	return id;
+	}
+
+	public void display(){
+		indi( "表示内容 :" + name );
+
+		double calcInTax = calcTax( price, tax );
+			indi( "税込み価格は" + calcInTax + "円です" );
+
+		calc.display();
+	}
+
+	public static double calcTax( double price, double tax ){
+	return( price * tax );
+	} 
+
 	@Override
 	public void execute() {
 		indi( "Lambda" );
@@ -65,57 +116,6 @@ public class Item03 implements Interface
 			indi( num + " " + str );
 			
 	}
-	
-	private String id;
-	private String name;
-	private double price;
-	private double tax;
-	private Calc03 calc;
-	
-	
-	public Item03 id( String id ){
-	this.id = id;
-	return this;
-	}
-	public Item03 name( String name ){
-	this.name = name;
-	return this;
-	}
-	public Item03 price( double price ){
-	this.price = price;
-	return this;
-	}
-	public Item03 tax( double tax ){
-	this.tax = tax;
-	return this;
-	}
-	public Item03 calc( Calc03 calc ) {
-		this.calc = calc;
-		return this;
-	}
-	
-	public static void save( Consumer<Item03> con ){
-	Item03 item = new Item03();
-	con.accept( item );
-	indi( "save :" + item );
-	}
-
-	public String getId(){
-	return id;
-	}
-
-	public void display(){
-		indi( "表示内容 :" + name );
-
-		double calcInTax = calcTax( price, tax );
-			indi( "税込み価格は" + calcInTax + "円です" );
-
-		calc.display();
-	}
-
-	public static double calcTax( double price, double tax ){
-	return( price * tax );
-	} 
 
 	@Override
 	public String toString(){
