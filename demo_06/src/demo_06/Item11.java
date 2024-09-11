7d80338f3f18705507f4b38e0cbc5f0cc39bbc89
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-09-11 09:42:17 +0900

    add.ArrayList

--- demo_06/src/demo_06/Item11.java
+++ demo_06/src/demo_06/Item11.java
@@ -1,15 +1,14 @@
 package demo_06;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.function.Consumer;
 
 public class Item11 implements Interface
-{
-	@Override
-	public void execute() {
-		indi( "Lambda" );
-
-	}
-	
+{	
 	private String id;
 	private String name;
 	private double price;
@@ -69,5 +68,59 @@ public class Item11 implements Interface
 	public static void indi( String s0 ) {
 		System.out.println( s0 );
 	}
-	
+	@Override
+	public void execute() {
+		indi( "Lambda" );
+		
+		ArrayList<String> animals = new ArrayList<String>( Arrays.asList( "Dog" ));
+		
+		ArrayList<String> animal = new ArrayList<String>();
+			animal.add( "Cat" );
+			animal.add( "Lion" );
+			animal.add( "Elephant" );
+		
+		String getAnimal = animal.get( 2 );
+			indi( getAnimal );
+		
+			animals.addAll( animal );
+			
+			animals.remove( 3 );
+			animals.add( 3, "Phoenix" );
+			
+			animals.set( 1, "Fox" );
+			
+		Comparator<String> compa = ( o1, o2 ) -> {
+			return( o2.compareTo( o1));
+		};
+			
+		Collections.sort( animals, compa );
+		
+		Iterator<String> itera = animals.iterator();
+		while( itera.hasNext()) {
+			String hoge = itera.next();
+				indi( hoge );
+		}
+		
+		ArrayList<Integer> seq = new ArrayList<Integer>();
+		
+		for( int i = 0; i < 5; i++ ) {
+			int number = new java.util.Random().nextInt( 4 ) + 1;
+				seq.add( number );
+		}
+		
+		for( int i : seq ) {
+			char[] base = { ' ', 'A', 'G', 'T', 'C' };
+				System.out.print( base[ i ] );
+		}
+		indi( "" );
+		
+		int num = new java.util.Random().nextInt( 10 ) + 1;
+		
+		String str = ( ( num % 2 ) == 0 )? ( ( num % 3 ) == 0 )?
+								"true/true" : "true/false"
+						:( ( num % 3 ) == 0 )?
+								"false/true" : "false/false" ;
+			indi( num + " " + str );
+			
+	}	
 }
