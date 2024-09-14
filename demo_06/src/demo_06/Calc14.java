f0ea1ac05928a46c5d9bf9c6ebff68e8f2b41f2b
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-09-14 19:59:39 +0900

    Test_09_14_home

---
+++ demo_06/src/demo_06/Calc14.java
@@ -0,0 +1,46 @@
+package demo_06;
+
+public class Calc14 extends Item14
+{
+	private String name;
+	
+	public Calc14( String name ) {
+		super();
+		this.name = name;
+	}
+	
+	public static Interface create() {
+		return() -> {
+			Calc14 calc = new Calc14( "Poly-morphism" );
+
+			Item14.save( item -> {
+				item.id( "掛け算" )
+					  .name( "税込み価格" )
+					  .price( 980 )
+					  .tax( 1.1 )
+					  .calc( calc );
+				
+				System.out.println( "計算式 :" + item.getId());
+				
+				item.display();
+				
+				item.execute();
+			}); 
+
+		};
+	}
+	
+	@Override
+	public void display() {
+		indi( name );
+	}
+	
+	public void process( Interface inter ) {
+		indi( "start" );
+		
+		inter.execute();
+		
+		indi( "end" );
+	}
+	
+}
