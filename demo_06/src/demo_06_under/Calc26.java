4bf05d2784448028ae67a6708550c60f9832941a
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-09-30 06:31:00 +0900

    Test_09_26_home

---
+++ demo_06/src/demo_06_under/Calc26.java
@@ -0,0 +1,47 @@
+package demo_06_under;
+
+import demo_06.Interface;
+
+public class Calc26 extends Item26
+{
+	private String name;
+	
+	public Calc26( String name ) {
+		super();
+		this.name = name;
+	}
+	
+	public static Interface create() {
+		return() -> {
+			Calc26 calc = new Calc26( "Poly-morphism" );
+
+			Item26.save( item -> {
+				item.id( "掛け算" )
+					  .name( "税込み価格" )
+					  .price( 980 )
+					  .tax( 1.1 )
+					  .calc( calc );
+				
+				System.out.println( "計算式 :" + item.getId());
+				
+				item.display();
+				
+				item.execute();
+			});
+
+		};
+	}
+	@Override
+	public void display() {
+		indi( name );
+	}
+	
+	public void process( Interface inter ) {
+		indi( "start" );
+		
+		inter.execute();
+		
+		indi( "start" );
+	}
+	
+}
