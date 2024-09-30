4989e1a3283f53990b45f5a0b704c4b25cffa28e
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-09-30 21:47:17 +0900

    Test_09_27_home

---
+++ demo_06/src/demo_06_under/Test_09_27_home.java
@@ -0,0 +1,18 @@
+package demo_06_under;
+
+public class Test_09_27_home 
+{
+	public static void main( String[] args )
+	{
+		Calc27 calc = new Calc27( "Poly-morphism" );
+		calc.process( () -> {
+			
+		});
+		
+		
+		
+		
+	}
+	
+	
+}
