8e698e710699fdc6bcd3d3a0032a854665cb2490
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-10-08 15:14:12 +0900

    <br/>

--- demo_06/src/demo_06_under/Test_09_30_home.java
+++ demo_06/src/demo_06_under/Test_09_30_home.java
@@ -4,7 +4,6 @@ import demo_06.Interface;
 
 public class Test_09_30_home 
 {
-	
 	public static void main( String[] args )
 	{
 		Calc30 calc = new Calc30( "Poly-morphism" );
@@ -14,9 +13,6 @@ public class Test_09_30_home
 			Interface save = Calc30.create();
 				save.execute();
 				
-				
 		});
-				
 	}
-	
 }
