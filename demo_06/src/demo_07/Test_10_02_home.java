9edb5c50421e16abe607ce564e3937156a70064b
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-10-09 10:43:35 +0900

    Test_10_02_home

---
+++ demo_06/src/demo_07/Test_10_02_home.java
@@ -0,0 +1,21 @@
+package demo_07;
+
+import demo_06.Interface;
+
+public class Test_10_02_home 
+{
+	public static void main( String[] args )
+	{
+		Calc02 calc = new Calc02( "Poly-morphims" );
+		calc.process( () -> {
+			System.out.println( "関心事を記述" );
+			
+			Interface save = Calc02.create();
+				save.execute();
+				
+				
+		});
+
+		
+	}
+}
