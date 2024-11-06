b5a86019eaa39a0a11bc7cbf9d972c3aa1034c61
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-11-06 11:27:41 +0900

    Test_11_06_home

---
+++ demo_06/src/demo_08/Test_11_06_home.java
@@ -0,0 +1,18 @@
+package demo_08;
+
+import demo_06.Interface;
+
+public class Test_11_06_home 
+{
+	public static void main(String[] args)
+	{
+		Calc06 calc = new Calc06("Poly-morphism");
+		calc.process(() -> {
+			System.out.println("関心事を記述");
+
+			Interface save = Calc06.create();
+			save.execute();
+
+		});
+	}
+}
