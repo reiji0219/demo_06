8f48fe2c5f133f7e20d537cdb0c0349d851139e2
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-11-12 12:09:44 +0900

    Test_10_24_home

---
+++ demo_06/src/demo_07/Test_10_24_home.java
@@ -0,0 +1,18 @@
+package demo_07;
+
+import demo_06.Interface;
+
+public class Test_10_24_home
+{
+	public static void main(String[] args) 
+	{
+		Calc24 calc = new Calc24("Poly-morphism");
+		calc.process(() -> {
+			System.out.println("関心事を記述");
+
+			Interface save = Calc24.create();
+			save.execute();
+
+		});
+	}
+}
