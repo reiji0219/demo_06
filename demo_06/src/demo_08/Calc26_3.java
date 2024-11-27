1b307478fbba500f79b50d6d815d6d68a0ae236f
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-11-27 20:58:11 +0900

    Test_11_26_ho

---
+++ demo_06/src/demo_08/Calc26_3.java
@@ -0,0 +1,45 @@
+package demo_08;
+
+import demo_06.Interface;
+
+public class Calc26_3 extends Item26 {
+	private String name;
+
+	public Calc26_3(String name) {
+		super();
+		this.name = name;
+	}
+
+	public static Interface create() {
+		return () -> {
+			Calc26_3 calc = new Calc26_3("Poly-morphism");
+
+			Item26_3.save(item -> {
+				item.id("掛け算")
+					  .name("税込み価格")
+					  .price(980)
+					  .tax(1.1)
+					  .calc(calc);
+
+				System.out.println("計算式 :" + item.getId());
+
+				item.display();
+
+				item.execute();
+			});
+		};
+	}
+
+	@Override
+	public void display() {
+		indi(name);
+	}
+
+	public void process(Interface inter) {
+		indi("start");
+
+		inter.execute();
+
+		indi("end");
+	}
+}
---
+++ demo_06/src/demo_08/Test_11_26_ho.java
@@ -0,0 +1,17 @@
+package demo_08;
+
+import demo_06.Interface;
+
+public class Test_11_26_ho 
+{
+	public static void main(String[] args) 
+	{
+		Calc26_3 calc = new Calc26_3("Poly-morphism");
+		calc.process(() -> {
+			System.out.println("関心事を記述");
+
+			Interface save = Calc26.create();
+			save.execute();
+		});
+	}
+}
