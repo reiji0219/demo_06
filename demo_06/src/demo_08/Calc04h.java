36951f81129aa6b50565811b1c3e216375bc6006
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-12-04 10:07:03 +0900

    Test_12_04_job

---
+++ demo_06/src/demo_08/Calc04h.java
@@ -0,0 +1,45 @@
+package demo_08;
+
+import demo_06.Interface;
+
+public class Calc04h extends Item04h {
+	private String name;
+
+	public Calc04h(String name) {
+		super();
+		this.name = name;
+	}
+
+	public static Interface create() {
+		return () -> {
+			Calc04h calc = new Calc04h("Poly-morphism");
+
+			Item04h.save(item -> {
+				item.id("掛け算")
+					  .name("税込み価格")
+					  .price(980)
+					  .tax(1.1)
+					  .calc(calc);
+
+				System.out.println("計算式 :" + item.getId());
+
+				item.display();
+				
+				item.execute();
+			});
+		};
+	}
+
+	@Override
+	public void display() {
+		indi(name);
+	}
+
+	public void process(Interface inter) {
+		indi("start");
+
+		inter.execute();
+
+		indi("end");
+	}
+}
