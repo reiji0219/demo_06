5b32ed03087535b85c4ff6d846a5995c9a712951
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-12-07 21:41:41 +0900

    Test_12_06_job

---
+++ demo_06/src/demo_08/Test_12_06_job.java
@@ -0,0 +1,19 @@
+package demo_08;
+
+import demo_06.Interface;
+
+public class Test_12_06_job
+{ 
+  public static void main( String[] args )
+  {
+    Calc06 calc = new Calc06( "" );
+    calc.process( () -> { 
+      System.out.println( "関心事を記述" );
+
+      Interface save = Calc06.create();
+        save.execute();
+        
+    });
+  }
+}
+
