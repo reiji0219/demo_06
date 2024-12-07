e093d25fa44b6b9c4e81a472f4ab7462a8842bb1
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-12-07 20:50:23 +0900

    Test_12_05_job

---
+++ demo_06/src/demo_08/Calc05.java
@@ -0,0 +1,45 @@
+package demo_08;
+
+import demo_06.Interface;
+
+public class Calc05 extends Item05 {
+	private String name;
+
+	public Calc05(String name) {
+		super();
+		this.name = name;
+	}
+
+	public static Interface create() {
+		return () -> {
+			Calc05 calc = new Calc05("Poly-morphism");
+
+			Item05.save(item -> {
+				item.id("掛け算")
+					  .name("税込み価格")
+					  .price(980)
+					  .tax(1.1)
+					  .calc(calc);
+
+				System.out.println("Hello World");
+
+				item.display();
+				
+				item.execute();
+			});
+		};
+	}
+
+	@Override
+	public void display() {
+		indi(name);
+	}
+
+	public void process(Interface inter) {
+		indi("start");
+
+		inter.execute();
+
+		indi("end");
+	}
+}
