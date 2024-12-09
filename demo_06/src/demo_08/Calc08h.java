31ed7286ced854b824854186c97eccaab940f169
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-12-09 20:27:40 +0900

    Test_12_08_home

---
+++ demo_06/src/demo_08/Calc08h.java
@@ -0,0 +1,45 @@
+package demo_08;
+
+import demo_06.Interface;
+
+public class Calc08h extends Item08h {
+	private String name;
+
+	public Calc08h(String name) {
+		super();
+		this.name = name;
+	}
+
+	public static Interface create() {
+		return () -> {
+			Calc08h calc = new Calc08h("Poly-morphism");
+
+			Item08h.save(item -> {
+				item.id("掛け算")
+					  .name("税込み価格")
+					  .price(980)
+					  .tax(1.1)
+					  .calc(calc);
+
+				System.out.println("計算式 :" + item.getId());
+
+				item.display();
+				
+				item.execute();
+			});
+		};
+	}
+
+	@Override
+	public void display() {
+		indi(name);
+	}
+
+	public void process(Interface inter) {
+		indi("start");
+
+		inter.execute();
+
+		indi("end");
+	}
+}
---
+++ demo_06/src/demo_08/Test_12_08_home.java
@@ -0,0 +1,19 @@
+package demo_08;
+
+import demo_06.Interface;
+
+public class Test_12_08_home
+{
+  public static void main( String[] args )
+  {
+    Calc08h calc = new Calc08h( "Poly-morphism" );
+    calc.process( () -> {
+      System.out.println( "関心事を記述" );
+
+      Interface save = Calc08h.create();
+        save.execute();
+
+    });
+  }
+}
+
