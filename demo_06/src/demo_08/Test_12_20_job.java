85a1b6ef55b957b7b4a6c24d527ea4c63a322c4c
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-12-25 23:47:32 +0900

    Test_12_20_job

---
+++ demo_06/src/demo_08/Test_12_20_job.java
@@ -0,0 +1,17 @@
+package demo_08;
+
+import demo_06.Interface;
+
+public class Test_12_20_job
+{ 
+  public static void main( String[] args )
+  { 
+    Calc20 calc = new Calc20( "Poly-morphism" );
+    calc.process( () -> { 
+      System.out.println( "関心事を記述" );
+
+      Interface save = Calc20.create();
+        save.execute();
+    });
+  }
+}
