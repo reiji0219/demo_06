dd0d5e3a01952fd75517127d800d0ac7dcac83b3
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-12-26 23:18:39 +0900

    Test_12_24b

---
+++ demo_06/src/demo_08/Item24b.java
@@ -0,0 +1,75 @@
+package demo_08;
+
+import java.util.function.Consumer;
+import demo_06.Interface;
+
+public class Item24b implements Interface {
+	@Override
+	public void execute() {
+		indi("Lambda");
+
+	}
+
+	private String id;
+	private String name;
+	private double price;
+	private double tax;
+	private Calc24b calc;
+
+	public Item24b id(String id) {
+		this.id = id;
+		return this;
+	}
+
+	public Item24b name(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public Item24b price(double price) {
+		this.price = price;
+		return this;
+	}
+
+	public Item24b tax(double tax) {
+		this.tax = tax;
+		return this;
+	}
+
+	public Item24b calc(Calc24b calc) {
+		this.calc = calc;
+		return this;
+	}
+
+	public static void save(Consumer<Item24b> con) {
+		Item24b item = new Item24b();
+		con.accept(item);
+		indi("save" + item);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void display() {
+		indi("表示内容 :" + name);
+
+		double calcInTax = calcTax(price, tax);
+		indi("税込み価格は" + calcInTax + "円です");
+
+		calc.display();
+	}
+
+	public static double calcTax(double price, double tax) {
+		return (price * tax);
+	}
+
+	@Override
+	public String toString() {
+		return ("Item24b :" + "id=" + id + " " + "name=" + name + " " + "price=" + price + " " + "tax=" + tax);
+	}
+
+	public static void indi(String s0) {
+		System.out.println(s0);
+	}
+}
