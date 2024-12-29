4fac4cd992a05e9bc54fb2d99e7c9ffff26125be
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-12-30 08:17:55 +0900

    Test_12_29c_home

---
+++ demo_06/src/demo_08/Test_12_29c_home.java
@@ -0,0 +1,18 @@
+package demo_08;
+
+import demo_06.Interface;
+
+public class Test_12_29c_home
+{
+  public static void main( String[] args )
+  {
+    Calc29c calc = new Calc29c( "Poly-morphism" );
+    calc.process( () -> {
+      System.out.println( "関心事を記述" );
+
+      Interface save = Calc29c.create();
+        save.execute();
+    });
+  }
+}
+
